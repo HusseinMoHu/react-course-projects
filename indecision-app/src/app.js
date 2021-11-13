274076c37809098e40a2bf062bbcfc2aa6759b11
Hussein Mohamed <hussseinm1818@gmail.com>
2021-11-13 23:44:31 +0200

    Events and methods

--- indecision-app/src/app.js
+++ indecision-app/src/app.js
@@ -27,19 +27,26 @@ class Header extends React.Component {
 }
 
 class Action extends React.Component {
+  handlePick() {
+    alert("handlePick");
+  }
   render() {
     return (
       <div>
-        <button>What should i do?</button>
+        <button onClick={this.handlePick}>What should i do?</button>
       </div>
     );
   }
 }
 
 class Options extends React.Component {
+  handleRemoveAll() {
+    alert("handleRemoveAll");
+  }
   render() {
     return (
       <ol>
+        <button onClick={this.handleRemoveAll}>Remove All</button>
         {this.props.options.map((option) => (
           <Option key={option} optionText={option} />
         ))}
@@ -55,10 +62,23 @@ class Option extends React.Component {
 }
 
 class AddOption extends React.Component {
+  handleAddOption(e) {
+    e.preventDefault();
+
+    // get input by name="option"
+    // trim input to remove spaces
+    const option = e.target.elements.option.value.trim();
+    if (option) {
+      alert(option);
+    }
+  }
   render() {
     return (
       <div>
-        <p>AddOption component here</p>
+        <form onSubmit={this.handleAddOption}>
+          <input type="text" name="option" />
+          <button>Add option</button>
+        </form>
       </div>
     );
   }
