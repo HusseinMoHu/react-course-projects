cb49ab416033781c15464119d6657f18da28a22f
Hussein Mohamed <hussseinm1818@gmail.com>
2021-11-13 16:22:23 +0200

    more components

--- indecision-app/src/app.js
+++ indecision-app/src/app.js
@@ -1,13 +1,50 @@
 class Header extends React.Component {
   render() {
-    return <p>This si from the header!</p>;
+    return (
+      <div>
+        <h1>Indecision App</h1>
+        <p>Put your life in the hands of a computer</p>
+      </div>
+    );
+  }
+}
+
+class Action extends React.Component {
+  render() {
+    return (
+      <div>
+        <button>What should i do?</button>
+      </div>
+    );
+  }
+}
+
+class Options extends React.Component {
+  render() {
+    return (
+      <div>
+        <p>Options component here</p>
+      </div>
+    );
+  }
+}
+
+class AddOption extends React.Component {
+  render() {
+    return (
+      <div>
+        <p>AddOption component here</p>
+      </div>
+    );
   }
 }
 
 const jsx = (
   <div>
-    <h1>Title</h1>
     <Header />
+    <Action />
+    <Options />
+    <AddOption />
   </div>
 );
 
